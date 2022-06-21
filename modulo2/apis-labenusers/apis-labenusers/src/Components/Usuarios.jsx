a9a4e0c76949c915c04f23ebe1246b98c99e28ae
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-06-21 20:15:06 -0300

    preferi parar para estudar react pq enquanto tava so no puro eu tava entendendo mas dai entrou no react....

---
+++ modulo2/apis-labenusers/apis-labenusers/src/Components/Usuarios.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import axios from "axios";
+
+export default class Usuarios extends React.Component {
+    state = {
+        name: " ",
+        email: " "
+    }
+
+    criarUsuario = () => {
+        axios
+        .get(
+            `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users`,
+            {
+                headers: {
+                    Authorization: "Laksmin-Bittencourt-Alves"
+                }
+
+            }
+        
+        )
+        .then((resposta) => {
+        this.setState({ nome: resposta.data.result});
+        })
+        .catch(() => {
+            console.log(erro.response);
+        });
+
+    };
+
+    componentDidMount() {
+        this.criarUsuario();
+    }
+
+    render () {
+        return (
+            <main>
+                <h4>{this.props.Usuarios.name}</h4>
+                {this.state.name.map((name) => {
+                    return <p key={nome.id}>{name.name}</p>
+                })}
+            </main>
+        );
+    
+    }
+
+}
