c25a4c361211d191576c8048541fd48ac040840d
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-06-23 20:00:31 -0300

    so fiz a estrutura do app, fiquei revisando tudo

---
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Styled from "styled-components";
+import Axios from "axios";
+
+export default class App extends Styled.Components {
+  state= {
+
+  }
+  
+}
+
+render() {
+  
+  return(
+    
+  )
+
+}
+
+  
+
+
