f4ee9d18dc89c27bf11a3c3b2d7639ba15d2ed9d
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-07-15 19:07:59 -0300

    comecei a fazer a requisição das listas...

--- modulo3/projeto-labex/labex/src/Pages/ListTripsPage.js
+++ modulo3/projeto-labex/labex/src/Pages/ListTripsPage.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import {useNavigate} from "react-router-dom";
 import {goBack, goToForm} from "../Routes/Cordinator";
+import axios from "axios";
 
 const Divzona = styled.div`
     border: solid black 2px;
@@ -17,13 +18,32 @@ const Divzona = styled.div`
 
 export default function ListTripsPage() {
 
+    const [ListTrip, setListTrip] = useState([])
+
     const navigate = useNavigate()
+
+    const getListTrip = (() => {
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labeX/laksmin/trips'
+        ).than((response) => {
+            setListTrip(response.data.trips)
+            console.log(response.date)
+        
+        }).catch((err) => {
+            console.log(err.response)
+        })
+
+    })
+
+
 
     return(
         <Divzona>
             <button onClick={() => goBack(navigate, "/")}>Voltar</button>
             <button onClick={() => goToForm(navigate, "Form")}>Inscrever-se</button>
             <p>Lista De Viagens</p>
+            <div>
+            
+            </div>
         </Divzona>
     )
 }
