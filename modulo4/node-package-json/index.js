c969e6baa74e43b79d777795a364caec2b4b1282
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-08-09 22:48:43 -0300

    exercicios-de-node

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,4 @@
+// Gente não vou conseguir fazer os exercicios, porque a tarde la pelas 17 fiquei sem internet
+// e me enrrolei toda de manhã pra fazer pq tava com medo dos comandos fiz bem devagar,
+// ao mesmo tempo que via o slide
+// a luz net voltou la pelas 8:45. ai fodeu mas vou fazer amanha prometo e estou entendendo tudo :)
