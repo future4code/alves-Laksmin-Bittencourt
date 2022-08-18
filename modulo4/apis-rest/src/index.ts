decbdad24f02ba6a66f2d88648e7202d412f1464
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-08-18 19:16:44 -0300

    agr deu um nozinho mas eu recupero

---
+++ modulo4/apis-rest/src/index.ts
@@ -0,0 +1,16 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import { users } from "./data";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+});
+
+app.get("/todoOsUsuarios", (req: Request, res: Response) => {
+    res.send(users)
+})
