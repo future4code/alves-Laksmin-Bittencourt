7634f4b94077a11e54e88b8ea67da8dde5636793
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-09-20 13:28:49 -0300

    error

--- modulo5CERTO/arquitetura-camadas/arquitetura-camadas/src/index.ts
+++ modulo5CERTO/arquitetura-camadas/arquitetura-camadas/src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from "dotenv"
-import { pingRouter } from './router/pingRouter'
-import { userRouter } from './router/userRouter'
+
 
 dotenv.config()
 
@@ -13,6 +12,3 @@ app.use(cors())
 app.listen(process.env.PORT || 3003, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
 })
-
-app.use("/ping", pingRouter)
-app.use("/users", userRouter)
